1078709b04a2eff442dd1a91c598fee04469bab6
Pedro <pedro@pepicrft.me>
2025-06-07 13:44:24 +0200

    Fix CI

--- mise/tasks/install.sh
+++ mise/tasks/install.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 # mise description="Installs the project dependencies"
 
-(cd $MISE_PROJECT_ROOT && bundle install)
-(cd $MISE_PROJECT_ROOT && mix deps.get)
-(cd $MISE_PROJECT_ROOT && vale sync)
+mix deps.get
+vale sync
